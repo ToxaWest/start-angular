e48d89df5dd0230b4494de82ba551ae7ba827318
Anton Zakharov <toxawest@gmail.com>
2018-06-11 16:26:15 +0300

    add login & register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialAppModule } from './ngmaterial.module';
 import { ExpandComponent } from './expand/expand.component';
-import { RouterModule } from '@angular/router';
 import { NgxEditorModule } from 'ngx-editor';
 // Components //
 import { AppComponent } from './app.component';
 import { TableComponent, TableModalDialogComponent} from './table-general-info/table-general-info';
 import { TableDyComponent, TableDyModalDialogComponent } from './table-dy/table-dy.component';
-import { LoginComponent } from './login/login.component';
 import {EditPdfTemplateComponent, SafeHtmlPipe} from './edit-pdf-template/edit-pdf-template.component';
 import {PdfToPrintTestComponent} from './pdf-to-print-test/pdf-to-print-test.component';
 import {TableStateOfClaimComponent} from './table-state-of-claim/table-state-of-claim.component';
@@ -21,6 +19,18 @@ import {PdfTemplateService} from './services/pdf-template.service';
 import {TableDyService} from './table-dy/table-dy.service';
 import {TableStateOfClaimService} from './table-state-of-claim/table-state-of-claim.service';
 
+import { fakeBackendProvider } from './_helpers/';
+import { routing } from './app.routing';
+
+import { AlertComponent } from './_directives/';
+import { AuthGuard } from './_guards/';
+import { JwtInterceptor } from './_helpers/';
+import { AlertService, AuthenticationService, UserService } from './_services/';
+import { HomeComponent } from './home/';
+import { LoginComponent } from './login/';
+import { RegisterComponent } from './register/';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,11 +39,14 @@ import {TableStateOfClaimService} from './table-state-of-claim/table-state-of-cl
     ExpandComponent,
     TableDyComponent,
     TableDyModalDialogComponent,
-    LoginComponent,
     SafeHtmlPipe,
     PdfToPrintTestComponent,
     EditPdfTemplateComponent,
     TableStateOfClaimComponent,
+    AlertComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
 ],
   imports: [
     BrowserModule,
@@ -42,39 +55,25 @@ import {TableStateOfClaimService} from './table-state-of-claim/table-state-of-cl
     MaterialAppModule,
     HttpClientModule,
     NgxEditorModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot([
-        {
-            path: '',
-            component: TableComponent
-        },
-        {
-            path: 'login',
-            component: LoginComponent
-        },
-        {
-            path: 'pdf-to-print',
-            component: PdfToPrintTestComponent
-        },
-        {
-            path: 'edit-template',
-            component: EditPdfTemplateComponent
-        },
-        {
-            path: 'notification',
-            component: TableDyComponent
-        },
-        {
-            path: 'claim_order_request',
-            component: TableStateOfClaimComponent
-        }
-    ])
+    routing,
+    BrowserAnimationsModule
   ],
   providers: [
-      TableBasicService,
-      PdfTemplateService,
-      TableStateOfClaimService,
-      TableDyService
+    TableBasicService,
+    PdfTemplateService,
+    TableStateOfClaimService,
+    TableDyService,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+    // provider used to create fake backend
+    fakeBackendProvider
   ],
   entryComponents: [TableModalDialogComponent, TableDyModalDialogComponent],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-export interface Person {
-    login: string;
-    pass: string;
-}
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService, AuthenticationService } from '../_services/index';
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
 })
+
 export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    returnUrl: string;
 
-  logined = false;
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
 
-  onSubmit(person: Person) {
-    this.logined = person.login === 'wise' && person.pass === 'wise';
-  }
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate([this.returnUrl]);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 }
